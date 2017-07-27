1e9868dfed56223ffe966e40ebbb1c4c98fc0378
special-wen <special_wen@qq.com>
2017-07-27 21:49:43 +0800

    node

---
+++ util.js
@@ -0,0 +1,36 @@
+/**
+ * Created by zxw on 17-7-27.
+ */
+'use static'
+var util = require('util');
+function Base() {
+    this.name = 'base';
+    this.base = 1991;
+    this.sayHello = function () {
+        console.log('Hello '+this.name);
+    };
+}
+Base.prototype.showName = function () {
+    console.log(this.name);
+};
+function Sub() {
+    this.name = 'sub';
+}
+util.inherits(Sub,Base);
+var objBase = new Base();
+objBase.showName();
+objBase.sayHello();
+console.log(objBase);
+var objSub = new Sub();
+objSub.showName();
+console.log(objSub);
+
+function Person() {
+    this.name = 'byvoid';
+    this.toString = function () {
+        return this.name;
+    };
+}
+var obj = new Person();
+console.log(util.inspect(obj));
+console.log(util.inspect(obj,true));
